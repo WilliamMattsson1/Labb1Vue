fc7ae49d022f16e23eb1ff7a9cdd338ebb1a8758
William Mattsson <wmattsson@hotmail.com>
2024-02-02 16:45:40 +0100

    Fixing structure

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import './assets/main.css'
+import './assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import 'bootstrap/dist/css/bootstrap.css' // importera bootstrap css stilar
+
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js' // Importera Bootstrap JS filer
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(bootstrap)
 
 app.mount('#app')
